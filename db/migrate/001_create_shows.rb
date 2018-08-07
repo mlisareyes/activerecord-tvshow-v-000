4ad19c935636af1f33c18fe0e046094f5baa08a6
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-07 11:06:05 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,10 +1,13 @@
-lass CreateShows < ActiveRecord::Migration
+class CreateShows < ActiveRecord::Migration
   def change
     create_table :shows do |t|
       t.string :name
-      t.string :network
       t.string :day
+      t.string :network
+
       t.integer :rating
+
+      t.timestamps null: false
     end
   end
 end
