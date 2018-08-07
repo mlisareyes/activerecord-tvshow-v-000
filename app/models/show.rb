9d1c9e75f5f2c90d3334ca544b17916f4067c730
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-08-07 11:08:44 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,30 +1,37 @@
 class Show < ActiveRecord::Base
 
-  def self.highest_rating
-    group("rating").order("rating DESC").limit(1)[0].rating
-  end
+    def self.highest_rating
+        best = Show.maximum(:rating)
+        best
+    end
 
-  def self.most_popular_show
-    self.find_by(rating: self.highest_rating)
-  end
+    def self.most_popular_show
+        rating = Show.highest_rating
+        show = Show.find_by(:rating => rating)
+        show
+    end
 
-  def self.lowest_rating
-    group("rating").limit(1)[0].rating
-  end
+    def self.lowest_rating
+        best = Show.minimum(:rating)
+        best
+    end
 
-  def self.least_popular_show
-    self.find_by(rating: self.lowest_rating)
-  end
+    def self.least_popular_show
+        rating = Show.lowest_rating
+        show = Show.find_by(:rating => rating)
+        show
+    end
 
-  def self.ratings_sum
-    sum("rating")
-  end
+    def self.ratings_sum
+        Show.sum(:rating)
+    end
 
-  def self.popular_shows
-    where("rating > 5")
-  end
+    def self.popular_shows
+        Show.where("rating >5")
+    end
 
-  def self.shows_by_alphabetical_order
-    group("name")
-  end
+    def self.shows_by_alphabetical_order
+        Show.all.order(:name)
+    end
+
 end
